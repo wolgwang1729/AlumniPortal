034d155b1dcc304b1385b46b11f4a39db1134911
wolgwang <145632038+wolgwang1729@users.noreply.github.com>
2024-11-11 20:39:34 +0530

    Refactor Home component to use imported image for Entrance instead of relative path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import EntranceImage from "../assets/Entrance.jpg";
 
 export default function Home() {
     return (
@@ -7,13 +7,13 @@ export default function Home() {
             <div className="relative mx-auto max-w-full">
                 <img
                     className="w-full h-auto"
-                    src="src\assets\Entrance.jpg"
+                    src={EntranceImage}
                     alt="DTU Entrance"
                 />
                 <div className="absolute inset-0 flex flex-col items-center justify-between text-white bg-black bg-opacity-50 pt-5 pb-56">
                     <h1 className="font-serif text-9xl font-bold mb-4">Welcome Home</h1>
                     <p className="font-serif text-3xl text-center max-w-5xl">
-                        Step into the virtual home of DTU’s vibrant community and DCE legacy —a place where innovation and excellence extend beyond the campus. Reconnect with classmates, explore opportunities, and stay engaged with the latest research and ideas from Delhi Technological University.
+                        Step into the virtual home of DTU’s vibrant community and DCE legacy — a place where innovation and excellence extend beyond the campus. Reconnect with classmates, explore opportunities, and stay engaged with the latest research and ideas from Delhi Technological University.
                     </p>
                 </div>
             </div>
