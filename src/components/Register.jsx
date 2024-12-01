5ac048d07df7e98f06e2417952b783826b94a125
wolgwang <145632038+wolgwang1729@users.noreply.github.com>
2024-12-01 09:47:06 +0530

    Refactor Login and Register components to improve image display and responsiveness

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,8 +7,8 @@ export default function Register() {
         <>
             <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
                 <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-                    <div className="flex ">
-                        <img src={LoginImage}   alt="" />
+                    <div className="hidden lg:w-auto lg:flex lg:rounded-lg">
+                        <img src={LoginImage}  className="rounded-lg" alt="" />
                     </div>
                     <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
                         <div className="flex items-center justify-center">
